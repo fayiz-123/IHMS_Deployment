24258e74f0620a3ff4de37d5512cd76d1aa03e43
fayiz-123 <muhammedfayiz59@gmail.com>
2025-08-18 21:00:32 +0530

    PWA Configuration- basic setup completed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Integrated Home Management System',
+      short_name: 'IHMS',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#7020f1',
+      theme_color: '#7020f1',
+      icons: [
+        { src: '/IHMS.png', sizes: '192x192', type: 'image/png' },
+        { src: '/IHMS.png', sizes: '512x512', type: 'image/png' }
+      ]
+    }
+  })],
+
 })
